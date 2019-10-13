2a49d4a4ef67013ad4fbf37e0d61e11ad75d3420
Hrishi <sakimon8@gmail.com>
2019-10-13 12:08:33 +0530

    jack

---
+++ app/src/main/java/com/example/announcements/updatedb.java
@@ -0,0 +1,57 @@
+package com.example.announcements;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class updatedb extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("Announcements");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot AnnSnapshot : dataSnapshot.getChildren()) {
+                    Announcements a = AnnSnapshot.getValue(Announcements.class);
+                    MainActivity.arrayContent.add(a.content);
+                    MainActivity.arraySubject.add(a.subject);
+                }
+                MainActivity.changedata();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),"Database Connection Error",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
